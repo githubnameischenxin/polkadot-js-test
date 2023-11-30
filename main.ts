2f5e723dd1c093db6f90a41d9d68e3b138caa091
chenxin <flyer.chenxin@gmail.com>
2023-11-30 23:56:17 +0800

    polkadot.js test

---
+++ main.ts
@@ -0,0 +1,42 @@
+import { ApiPromise, WsProvider } from "@polkadot/api";
+import "@polkadot/api-augment";
+var sleep = (ms: number) => new Promise(r => setTimeout(r, ms));
+
+const WEB_SOCKET = "ws://127.0.0.1:9944";
+const connect = async () => {
+    const wsProvider = new WsProvider(WEB_SOCKET);
+    const api = await ApiPromise.create({provider: wsProvider});
+    await api.isReady;
+    return api;
+}
+
+const subscribe = async (api: ApiPromise) => {
+    await api.query.system.events(events => {
+        events.forEach(record => {
+            const event = record.event;
+            if(event.method === "SomethingStored") {
+                console.log("something is ", event.data[0].toHuman());
+                console.log("SomethingStored : ", record.toHuman());
+            }
+        });
+    });
+}
+
+const main = async () => {
+    const api = await connect();
+
+    
+    await subscribe(api);
+    
+    await sleep(50000);
+
+    console.log("function main");
+}
+
+main().then(() => {
+    console.log("exits with success");
+    process.exit(0);
+}).catch(err => {
+    console.error("error is ", err);
+    process.exit(1);
+});
